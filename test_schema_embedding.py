988f94d1ff202c5417b12adb135b124792ac7493
Shivam Mishra <shivaymishraeee@gmail.com>
2025-03-09 02:45:14 +0530

    Change the embedding model

--- test_schema_embedding.py
+++ test_schema_embedding.py
@@ -1,12 +1,43 @@
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings 
+# from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 import os
+import re
+import numpy as np
+import json
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+def sql_to_json():
+    # Read the SQL file
+    with open("./files/data.sql", "r", encoding="utf-8") as file:
+        sql_content = file.read()
+
+    # Regex pattern to match CREATE TABLE statements
+    table_pattern = re.findall(r"CREATE TABLE IF NOT EXISTS\s+(\w+)\s*\((.*?)\);", sql_content, re.DOTALL)
+
+    # Function to remove inline comments
+    def remove_inline_comments(schema):
+        return re.sub(r"--.*", "", schema).strip()
+
+
+    # Store tables in a numpy array
+    # table_schemas = np.array([{"table_name": name, "schema": schema.strip()} for name, schema in table_pattern])
+    table_schemas = np.array([
+        {"table_name": name, "schema": remove_inline_comments(schema)}
+        for name, schema in table_pattern
+    ])
+    # Convert numpy array to a list and save it as JSON
+    with open("table_schemas.json", "w", encoding="utf-8") as json_file:
+        json.dump(table_schemas.tolist(), json_file, indent=4)
+
+sql_to_json()
 
 def create_vector_db(documents, embedding_model):
     """
@@ -46,16 +77,17 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=50
 final_document = text_splitter.split_documents([sql_document])
 
 # Initialize embedding model
-embedding = HuggingFaceBgeEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5",
-    model_kwargs={"device": "cpu"},
-    encode_kwargs={"normalize_embeddings": True}
-)
+# embedding = HuggingFaceBgeEmbeddings(
+#     model_name="BAAI/bge-small-en-v1.5",
+#     model_kwargs={"device": "cpu"},
+#     encode_kwargs={"normalize_embeddings": True}
+# )
+embadding= GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
 
 def wrapper():
     # # Create vector database instance
-    vector_store = create_vector_db(final_document, embedding)
+    vector_store = create_vector_db(final_document, embadding)
     return vector_store
 # def get_json_data():
     
